aec8abcf451a4590f920384c69caa4a48ac07087
Godfather021 <MYWAY_AUST@YAHOO.COM>
2015-12-31 10:43:14 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -246,78 +246,78 @@ function create_config( )
     help_text = [[
 Commands list :
 
-!kick [username|id]
+kick [username|id]
 You can also do it by reply
 
-!ban [ username|id]
+ban [ username|id]
 You can also do it by reply
 
-!unban [id]
+unban [id]
 You can also do it by reply
 
-!who
+who
 Members list
 
-!modlist
+modlist
 Moderators list
 
-!promote [username]
+promote [username]
 Promote someone
 
-!demote [username]
+demote [username]
 Demote someone
 
-!kickme
+kickme
 Will kick user
 
-!about
+about
 Group description
 
-!setphoto
+setphoto
 Set and locks group photo
 
-!setname [name]
+setname [name]
 Set group name
 
-!rules
+rules
 Group rules
 
-!id
+id
 return group id or user id
 
-!help
+help
 
-!lock [member|name|bots]
+lock [member|name|bots]
 Locks [member|name|bots] 
 
-!unlock [member|name|photo|bots]
+unlock [member|name|photo|bots]
 Unlocks [member|name|photo|bots]
 
-!set rules <text>
+set rules <text>
 Set <text> as rules
 
-!set about <text>
+set about <text>
 Set <text> as about
 
-!settings
+settings
 Returns group settings
 
-!newlink
+newlink
 create/revoke your group link
 
-!link
+link
 returns group link
 
-!owner
+owner
 returns group owner id
 
-!setowner [id]
+setowner [id]
 Will set id as owner
 
-!setflood [value]
+setflood [value]
 Set [value] as flood sensitivity
 
-!stats
+stats
 Simple message statistics
 
 !save [value] <text>
@@ -326,20 +326,20 @@ Save <text> as [value]
 !get [value]
 Returns text of [value]
 
-!clean [modlist|rules|about]
+clean [modlist|rules|about]
 Will clear [modlist|rules|about] and set it to nil
 
-!res [username]
+res [username]
 returns user id
 "!res @username"
 
-!log
+log
 will return group logs
 
-!banlist
+banlist
 will return group ban list
 
-**U can use both "/" and "!" 
+
 
 
 *Only owner and mods can add bots in group
